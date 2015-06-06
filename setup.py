4215e4b1d6bc3dda83f33448442fac605c5af0e3
Lars Wallenborn <lars@wallenborn.net>
2015-06-06 18:09:33 +0200

    add setup py with test dependencies

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+version = "0.0.1"
+
+setup(
+    name='TwichPlaysChess',
+    version=version,
+    description='TODO',
+    author='TODO',
+    author_email='TODO',
+    license='TODO',
+    url='https://github.com/donfussl/TwitchPlaysChess',
+    packages=find_packages(),
+    test_suite='test_pieces',
+    tests_require=['pytest']
+)
